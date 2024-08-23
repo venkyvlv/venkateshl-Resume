b674f95a96f46cc0ee5fe9e2f9e815ed4eadacfb
Venkatesh.L <53553456+venkygitvlv@users.noreply.github.com>
2024-08-23 16:51:25 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import Home from "./components/Home";
 import About from "./components/About";
+import SnakeGame from "./components/BounceGame";
 import "./App.css";
-// import Services from './components/Services';
-// import Portfolio from './components/Portfolio';
-// import Clients from './components/Clients';
-// import Blog from './components/Blog';
-// import Contact from './components/Contact';
 
 const App = () => {
   return (
@@ -17,12 +13,9 @@ const App = () => {
         <Route path="/" element={<Profile />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          {/*<Route path="services" element={<Services />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="clients" element={<Clients />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="contact" element={<Contact />} /> */}
         </Route>
+        <Route path="/snake-game" element={<SnakeGame />} />
+        {/* Add other routes here */}
       </Routes>
     </Router>
   );
